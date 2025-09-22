58b9bd3c8713af67ca16ff37b9b801c8b6e95a80
alexpipe31w <alexpipe31w@gmail.com>
2025-09-22 09:14:06 -0500

    fix: conectar formulario de contacto con backend en Render

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -13,7 +13,7 @@ import { useState } from "react";
         message: formData.get("message"),
         };
 
-        const res = await fetch("http://localhost:5000/send-email", {
+        const res = await fetch("https://mi-portafolio-backend-nxii.onrender.com/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
